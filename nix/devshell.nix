ced028433ab88acc2b25fd35e98753c88c38faf3
Jonathan Andersson <zeapo@pm.me>
2026-02-18 20:17:58 +0700

    Add python + toml to devshell

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -7,6 +7,14 @@
   }: {
     devShells.default = pkgs.mkShell {
       inputsFrom = [self'.packages.default];
+
+      packages = with pkgs; [
+        (python3.withPackages (python-pkgs:
+          with python-pkgs; [
+            toml
+          ]))
+      ];
+
       inherit env;
     };
   };
